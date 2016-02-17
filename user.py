ff17a85c7bc1a35e5a096cc45abe69ecf7c64e15
2Cubed <2CubedBot@gmail.com>
2016-02-17 18:37:23 -0500

    Replace individual user request functions with global request

--- user.py
+++ user.py
@@ -4,34 +4,16 @@ from requests import Session
 class User:
     path = path = "https://beam.pro/api/v1"
 
-    def getChannelID(self, username):
-        userJSON = self.session.get(self.path + "/channels/{user}".format(
-            user=username)).json()
-
-        return self.session.get(userJSON.get('id'))
-
     def __init__(self):
         self.session = Session()
 
-    def get(self, url, **kwargs):
-        return self.session.get(self.path + url, **kwargs)
+    def request(self, req, url, *args, **kwargs):
+        if req.lower() in ('get', 'head', 'post', 'put', 'delete', 'options'):
+            return self.session.__getattribute__(req.lower())(
+                self.path + url, *args, **kwargs
+            )
 
-    def head(self, url, **kwargs):
-        return self.session.head(self.path + url, **kwargs)
-
-    def post(self, url, data=None, **kwargs):
-        return self.session.post(self.path + url, data, **kwargs)
-
-    def put(self, url, data=None, **kwargs):
-        return self.session.put(self.path + url, data, **kwargs)
-
-    def delete(self, url, **kwargs):
-        return self.session.delete(self.path + url, **kwargs)
-
-    def options(self, url, **kwargs):
-        return self.session.options(self.path + url, **kwargs)
-
-    def login(self, username, password, code):
+    def login(self, username, password, code=''):
         """Authenticate and login with Beam."""
         auth = {
             "username": username,
