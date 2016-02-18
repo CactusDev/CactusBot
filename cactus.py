872b84f60f65d0eec4be76b3924255f19a0f0c7e
2Cubed <2CubedBot@gmail.com>
2016-02-17 23:28:06 -0500

    Implement logging

--- cactus.py
+++ cactus.py
@@ -3,25 +3,31 @@
 from user import User
 from json import load
 
-import logging
-
 
 class Cactus(User):
-    def __init__(self, debug=False):
-        super(Cactus, self).__init__()
-        self.debug = debug
+    def __init__(self, **kwargs):
+        super(Cactus, self).__init__(**kwargs)
+        self.debug = kwargs.get("debug", False)
+        self.autorestart = kwargs.get("autorestart", True)
 
-    def load_config(self, filename="config.json"):
+    def load_config(self, filename):
         with open(filename) as config:
             self.config = load(config)
             self.channel_data = self.login(**self.config)
             self.username = self.channel_data['username']
 
-            self.login(self.username, self.config['password'])
+    def run(self, config_file="config.json"):
+        try:
+            self.load_config(filename=config_file)
+            self.logger.info("Authenticated as: {}.".format(self.username))
+        except KeyboardInterrupt:
+            self.logger.info("Removing thorns... done.")
+            self.logger.info("CactusBot deactivated.")
+        except Exception:
+            self.logger.critical("Oh no, I crashed!")
+            if self.autorestart:
+                self.logger.info("Restarting...")
+                self.run(config_file=config_file)
 
-    def run(self):
-        self.load_config()
-        logging.info("Authenticated as: {user}.".format(user=self.username))
-
-cactus = Cactus()
+cactus = Cactus(debug=True)
 cactus.run()
--- user.py
+++ user.py
@@ -1,17 +1,42 @@
 from requests import Session
+from logging import getLogger
 
 
 class User:
     path = path = "https://beam.pro/api/v1"
 
-    def __init__(self):
+    def __init__(self, debug="WARNING"):
         self.session = Session()
+        self._init_logger(debug)
+
+    def _init_logger(self, level):
+        self.logger = getLogger('CactusBot')
+        if level is True:
+            level = "DEBUG"
+        levels = ("CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG", "NOTSET")
+        if level in levels:
+            level_num = __import__("logging").__getattribute__(level)
+            self.logger.setLevel(level_num)
+            try:
+                from coloredlogs import install
+                install(level=level)
+            except ImportError:
+                self.logger.warning(
+                    "Module 'coloredlogs' unavailable; using ugly logging.")
+        else:
+            self.logger.warn("Invalid logger level: {}".format(level))
+        self.logger.info("Logger initialized!")
 
     def request(self, req, url, *args, **kwargs):
         if req.lower() in ('get', 'head', 'post', 'put', 'delete', 'options'):
-            return self.session.__getattribute__(req.lower())(
+            response = self.session.__getattribute__(req.lower())(
                 self.path + url, *args, **kwargs
             )
+            if 'error' in response.json().keys():
+                self.logger.warn("Error: {}".format(response.json()['error']))
+            return response.json()
+        else:
+            self.logger.debug("Invalid request: {}".format(req))
 
     def login(self, username, password, code=''):
         """Authenticate and login with Beam."""
@@ -21,10 +46,7 @@ class User:
             "code": code
         }
 
-        channel_data = self.request("POST", "/users/login", auth).json()
-
-        if 'error' in channel_data.keys():
-            raise Exception(channel_data.get('message', ''))
+        channel_data = self.request("POST", "/users/login", auth)
 
         return channel_data
 
