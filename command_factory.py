240a1e0aa95f5a1c315d582af88a8fb38c7fb193
Innectic <innectic@gmail.com>
2016-02-20 01:09:07 -0800

    Finished command_factory

--- command_factory.py
+++ command_factory.py
@@ -1,14 +1,37 @@
 
+import sqlite3 as sql
+
+
+conn = sql.connect('data/bot.db')
+c = conn.cursor()
+
+
 class Command():
 
-    def get_response(cmd):
-        pass
+    def get_response(self, cmd):
+        return c.execute('SELECT response WHERE command={command};', command=cmd)
 
-    def set_response(cmd, resp):
-        pass
+    def set_response(self, cmd, resp):
+        access = self.get_response(cmd)
 
-    def remove(cmd):
-        pass
+        self.remove_command(cmd)
+        self.add_command(cmd, resp, access)
 
-    def add_command(cmd, resp, access):
-        pass
+    def add_command(self, cmd, response, access):
+        c.execute("INSERT INTO commands VALUES ({cmd}, {resp}, {access});".format(cmd=cmd, resp=response, access=access))
+
+        conn.commit()
+        conn.close()
+
+    def remove_command(self, cmd):
+        c.execute("DELETE FROM commands WHERE command={cmd};'".format(cmd=cmd))
+
+        conn.commit()
+        conn.close()
+
+    def change_command(self, cmd, resp, access):
+        c.execute("DELETE FROM command WHERE command={cmd};".format(cmd=cmd))
+        c.execute("INSERT INTO command VALUES ({cmd}, {resp}, {access});".format(cmd=cmd, resp=resp, access=access))
+
+        conn.commit()
+        conn.close()
