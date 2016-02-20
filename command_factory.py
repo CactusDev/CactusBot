d66c4448b219951cc695d24a008c664a5db88f90
Innectic <innectic@gmail.com>
2016-02-20 00:26:19 -0800

    Outline for command_factory

---
+++ command_factory.py
@@ -0,0 +1,14 @@
+
+class Command():
+
+    def get_response(cmd):
+        pass
+
+    def set_response(cmd, resp):
+        pass
+
+    def remove(cmd):
+        pass
+
+    def add_command(cmd, resp, access):
+        pass
