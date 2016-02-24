993c5f909f5dd5104aa565d728b4b759f9209f86
RPiAwesomeness <keyboardmailtesting@gmail.com>
2016-02-24 17:44:46 -0500

    Fixed print_cactus()

--- cactus.py
+++ cactus.py
@@ -20,8 +20,8 @@ def print_cactus():
  ,,,  ;`,',;
 ;,` ; ;' ` ;   ,',
 ;`,'; ;`,',;  ;,' ;        ------ /------\  ------ --------  |       |    -----
-;',`; ;` ' ; ;`'`';        |      |      |  |         |      |       |   \
-;` '',''` `,',`',;         |      |      |  |         |      |       |    ----\
+;',`; ;` ' ; ;`'`';        |      |      |  |         |      |       |   \\
+;` '',''` `,',`',;         |      |      |  |         |      |       |    ----\\
  `''`'; ', ;`'`'           |      |------|  |         |      |       |        |
       ;' `';               ------ |      |  ------    |      ---------   -----/
       ;` ' ;
@@ -39,26 +39,26 @@ class Cactus(User):
 
     def __init__(self, autorestart=True, **kwargs):
         super(Cactus, self).__init__(**kwargs)
-        self.debug = kwargs.get('debug', False)
+        self.debug = kwargs.get("debug", False)
         self.autorestart = autorestart
 
     def check_db(self):
-        if exists('data/bot.db'):
+        if exists("data/bot.db"):
             self.logger.info("Found database.")
         else:
             self.logger.info("Database wasn't found.")
             self.logger.info("Creating and setting defaults...")
 
-            conn = sql.connect('data/bot.db')
+            conn = sql.connect("data/bot.db")
             c = conn.cursor()
 
-            c.execute('''CREATE TABLE commands
-                (command text, response text,  access text)''')
+            c.execute("""CREATE TABLE commands
+                (command text, response text,  access text)""")
 
-            c.execute('''CREATE TABLE bot
-                (joinTime text, joinDate text, different text, total text)''')
+            c.execute("""CREATE TABLE bot
+                (joinTime text, joinDate text, different text, total text)""")
 
-            c.execute('''INSERT INTO bot VALUES('{time}, {date}, '0', '0')'''.format(time=strftime("%I-%M-%S-%Z"), date=strftime("%a-%B-%Y")))
+            c.execute("""INSERT INTO bot VALUES("{time}, {date}, "0", "0"")""".format(time=strftime("%I-%M-%S-%Z"), date=strftime("%a-%B-%Y")))
 
             conn.commit()
             conn.close()
@@ -72,9 +72,9 @@ class Cactus(User):
             self.logger.info("Config file was found. Loading...")
             with open(filename) as config:
                 self.config = load(config)
-                auth = {n: self.config[n] for n in ('username', 'password')}
+                auth = {n: self.config[n] for n in ("username", "password")}
                 self.channel_data = self.login(**auth)
-                self.username = self.channel_data['username']
+                self.username = self.channel_data["username"]
         else:
             self.logger.warn("Config file was not found. Creating...")
             copyfile("data/config-template.json", filename)
@@ -97,7 +97,7 @@ class Cactus(User):
                 self.autorestart = False
             except Exception:
                 self.logger.critical("Oh no, I crashed!")
-                self.logger.error('\n\n' + format_exc())
+                self.logger.error("\n\n" + format_exc())
 
                 if self.autorestart:
                     self.logger.info("Restarting in 10 seconds...")
@@ -114,10 +114,10 @@ class Cactus(User):
         self.load_config(filename=config_file)
         self.logger.info("Authenticated as: {}.".format(self.username))
 
-        channel = self.get_channel(self.config['channel'])
-        status = {True: "online", False: "offline"}[channel.get('online')]
+        channel = self.get_channel(self.config["channel"])
+        status = {True: "online", False: "offline"}[channel.get("online")]
         self.logger.info("Channel {ch} (id {id}) is {status}.".format(
-            ch=channel['token'], id=channel['id'], status=status
+            ch=channel["token"], id=channel["id"], status=status
         ))
 
 cactus = Cactus(debug=True, autorestart=False)
