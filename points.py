b33cd4421c9f7250d001f91cc24b1c2d540fb0e5
Innectic <innectic@gmail.com>
2016-02-28 09:29:53 -0800

    Start on point system

---
+++ points.py
@@ -0,0 +1,14 @@
+
+class Points():
+
+    def auto_points(self):
+        pass
+
+    def remove_points(self, user, amt):
+        pass
+
+    def add_points(self, user, amt):
+        pass
+
+    def set_points(self, user, amt):
+        pass
