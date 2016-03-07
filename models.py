8c235e58cd067c7b29a3cf27bbd3d418e0d3c511
2Cubed <2CubedBot@gmail.com>
2016-03-06 23:31:54 -0500

    Fix command storing

--- models.py
+++ models.py
@@ -41,7 +41,9 @@ class Quote(Base):
     author = Column(Integer)
 
 
-class Command(User, StoredCommand):
+class Command(StoredCommand):
+    user = User()
+
     def __call__(self, user, *args):
         response = self.response
 
@@ -146,7 +148,7 @@ class QuoteCommand(Command):
 
 class SocialCommand(Command):
     def __call__(self, args, data=None):
-        s = self.get_channel(data["channel"])["user"]["social"]
+        s = self.user.get_channel(data["channel"])["user"]["social"]
         a = [arg.lower() for arg in args[1:]]
         if s:
             if not a:
@@ -195,7 +197,7 @@ class ScheduleCommand(Command):
 
 class WhoAmICommand(Command):
     def __call__(self, args, data=None):
-        return 'Ohai, {name}! Want a :cactus ?'.format(name=self.get_channel(data["channel"], fields="token")["token"])
+        return self.user.get_channel(data["channel"], fields="token")["token"]
 
 
 class UptimeCommand(Command):
