e7f24c7d754c1a7c2475040f1e111d0900805209
2Cubed <2CubedBot@gmail.com>
2016-05-23 02:21:44 -0400

    Implement basic run.py

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-from CactusBot import Cactus, Beam
+from coloredlogs import install
 
-Cactus(Beam).run()
+from config import SERVICE
+from asyncio import get_event_loop
+
+
+async def run():
+    async with SERVICE as beam:
+        await beam._authenticate()
+        await beam.read()
+
+install(level="DEBUG")
+
+get_event_loop().run_until_complete(run())
