f54b66c55ee7dc0a6c467c68539e75e1465c4cdb
2Cubed <2CubedBot@gmail.com>
2016-06-10 03:26:49 -0400

    Move @subcommand to @Command.subcommand
    Automatically set default command name based on class name.

--- cactusbot/command.py
+++ cactusbot/command.py
@@ -1,4 +1,4 @@
-"""The brains of the command system."""
+"""The inner workings of the command system."""
 
 from inspect import signature
 
@@ -7,77 +7,6 @@ from functools import wraps
 import re
 
 from logging import getLogger
-
-
-def subcommand(function):
-    """Decorate a subcommand."""
-
-    params = list(signature(function).parameters.values())
-
-    @wraps(function)
-    def wrapper(self, *args, **data):
-        """Parse subcommand data."""
-
-        args = list(args)
-
-        args_params = tuple(
-            p for p in params if p.kind is p.POSITIONAL_OR_KEYWORD)
-        star_param = next(
-            (p for p in params if p.kind is p.VAR_POSITIONAL), None)
-
-        data_params = tuple(
-            p for p in params if p.kind is p.KEYWORD_ONLY)
-        kwargs = dict((p.name, data.get(p.annotation)) for p in data_params)
-
-
-        arg_range = (
-            len(tuple(
-                p for p in args_params if p.default is p.empty
-            )) + bool(star_param),
-            float('inf') if star_param else len(args_params)
-        )
-
-        if not arg_range[0] <= len(args) <= arg_range[1]:
-
-            if star_param is not None:
-                args_params += (star_param,)
-            syntax = "!{command} {subcommand} {params}".format(
-                command=self.__command__, subcommand=function.__name__,
-                params='<'+'> <'.join(p.name for p in args_params[1:])+'>'
-            )
-
-            if len(args) < arg_range[0]:
-                return "Not enough arguments. ({})".format(syntax)
-            elif len(args) > arg_range[1]:
-                return "Too many arguments. ({})".format(syntax)
-
-        for index, argument in enumerate(args_params[:len(args)]):
-            annotation = argument.annotation
-            if annotation is not argument.empty:
-                if isinstance(annotation, str):
-                    if annotation.startswith('?'):
-                        annotation = self.expressions.get(annotation[1:], '')
-                    match = re.match('^'+annotation+'$', args[index])
-                    if match is None:
-                        return "Invalid {type}: '{value}'.".format(
-                            type=argument.name, value=args[index])
-                    elif match.groups():
-                        args[index] = match.group(1)
-                else:
-                    self.logger.warning(
-                        "Invalid regex: '%s.%s.%s : %s'.",
-                        self.__command__, function.__name__,
-                        argument.name, annotation
-                    )
-
-        return function(
-            self, *args[1:],
-            **kwargs
-        )
-
-    wrapper.is_subcommand = True
-
-    return wrapper
 
 
 class CommandMeta(type):
@@ -95,11 +24,84 @@ class CommandMeta(type):
 class Command(metaclass=CommandMeta):
     """Parent all command classes."""
 
+    __command__ = None
+
     def __init__(self):
         self.logger = getLogger(__name__)
-        self.expressions = {
+
+        self.regex = {
             "username": r'@?([A-Za-z0-9]{,32})'
         }
+
+        if self.__command__ is None:
+            self.__command__ = type(self).__name__.lower()
+
+    @staticmethod
+    def subcommand(function):
+        """Decorate a subcommand."""
+
+        params = list(signature(function).parameters.values())
+
+        @wraps(function)
+        def wrapper(self, *args, **data):
+            """Parse subcommand data."""
+
+            args = list(args)
+
+            args_params = tuple(
+                p for p in params if p.kind is p.POSITIONAL_OR_KEYWORD)
+            star_param = next(
+                (p for p in params if p.kind is p.VAR_POSITIONAL), None)
+
+            data_params = tuple(
+                p for p in params if p.kind is p.KEYWORD_ONLY)
+            kwargs = {p.name: data.get(p.annotation) for p in data_params}
+
+            arg_range = (
+                len(tuple(
+                    p for p in args_params if p.default is p.empty
+                )) + bool(star_param),
+                float('inf') if star_param else len(args_params)
+            )
+
+            if not arg_range[0] <= len(args) <= arg_range[1]:
+
+                if star_param is not None:
+                    args_params += (star_param,)
+                syntax = "!{command} {subcommand} {params}".format(
+                    command=self.__command__, subcommand=function.__name__,
+                    params='<'+'> <'.join(p.name for p in args_params[1:])+'>'
+                )
+
+                if len(args) < arg_range[0]:
+                    return "Not enough arguments. ({})".format(syntax)
+                elif len(args) > arg_range[1]:
+                    return "Too many arguments. ({})".format(syntax)
+
+            for index, argument in enumerate(args_params[:len(args)]):
+                annotation = argument.annotation
+                if annotation is not argument.empty:
+                    if isinstance(annotation, str):
+                        if annotation.startswith('?'):
+                            annotation = self.regex.get(annotation[1:], '')
+                        match = re.match('^'+annotation+'$', args[index])
+                        if match is None:
+                            return "Invalid {type}: '{value}'.".format(
+                                type=argument.name, value=args[index])
+                        elif match.groups():
+                            args[index] = match.group(1)
+                    else:
+                        self.logger.warning(
+                            "Invalid regex: '%s.%s.%s : %s'.",
+                            self.__command__, function.__name__,
+                            argument.name, annotation
+                        )
+
+            return function(self, *args[1:], **kwargs)
+
+        wrapper.is_subcommand = True
+
+        return wrapper
 
     def __call__(self, *args, **data):
         # TODO: default subcommands
