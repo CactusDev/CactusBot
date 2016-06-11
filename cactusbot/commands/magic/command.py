e47e01bc95e9e0313aa9e4d74b8080411f956bf3
2Cubed <2CubedBot@gmail.com>
2016-06-11 05:16:41 -0400

    Fix Meta formatting

--- cactusbot/commands/magic/command.py
+++ cactusbot/commands/magic/command.py
@@ -10,18 +10,20 @@ class Meta(Command):
     __command__ = "command"
 
     @Command.subcommand
-    async def add(self, name: r'!?([+$]?)(.+)', *response, user: "username"):
+    async def add(self, name: r'!?([+$]?)(.+)', *response,
+                  added_by: "username"):
         """Add a command."""
         self.api.add_command(
             name, ' '.join(response),
-            addedBy=user
+            added_by=added_by
         )
         return "Added command !{}.".format(name)
 
     @Command.subcommand
-    async def remove(self, name: "?command"):
+    async def remove(self, name: "?command", *, removed_by: "username"):
+        """Remove a command."""
         try:
-            self.api.remove_command(name)
+            self.api.remove_command(name, removed_by=removed_by)
         except Exception:  # TODO
             return "Command !{} does not exist!".format(name)
         else:
@@ -29,6 +31,7 @@ class Meta(Command):
 
     @Command.subcommand
     async def list(self):
+        """List all custom commands."""
         commands = self.api.get_command()
         if commands:
             return "Commands: {}.".format(
