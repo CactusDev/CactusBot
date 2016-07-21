3f654d4ae9ad080f2aa776322d1379004ad78403
innectic <innectic@gmail.com>
2016-07-20 21:37:08 -0700

    Fix conf for docs

--- docs/conf.py
+++ docs/conf.py
@@ -36,8 +36,7 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
@@ -47,8 +46,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'CactusBot'
-copyright = '2016, Innectic 2Cubed ParadigmShift3d'
-author = 'Innectic 2Cubed ParadigmShift3d'
+copyright = '2016 CactusDev'
+author = 'CactusDev'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
