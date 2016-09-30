e8548c26df021d9eff7c056338e3442beeed9397
2Cubed <2Cubed@users.noreply.github.com>
2016-09-30 16:10:51 -0400

    Move for loop to generator comprehension

--- cactusbot/handlers/spam.py
+++ cactusbot/handlers/spam.py
@@ -16,11 +16,10 @@ class SpamHandler(Handler):
 
     def on_message(self, packet):
         """Handle message events."""
-        built_message = ""
-        for chunk in packet:
-            if chunk["type"] == "text":
-                built_message += chunk["text"]
-        exceeds_caps = self.check_caps(built_message)
+
+        exceeds_caps = self.check_caps(''.join(
+            chunk["text"] for chunk in packet if chunk["type"] == chunk["text"]
+        ))
         contains_emotes = self.check_emotes(packet)
         has_links = self.check_links(packet)
 
