143db7029b58b154a33c8b808bb97c0b86979bc1
Innectic <innectic@gmail.com>
2016-09-29 19:56:11 -0700

    Remove testing files

--- a.py
+++
@@ -1,7 +0,0 @@
-from cactusbot.handler import Handlers
-from cactusbot.handlers.command import CommandHandler
-
-a = Handlers(CommandHandler())
-b = a.handle("message", "potato")
-for thing in b:
-    print(thing)
