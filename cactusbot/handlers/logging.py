c77c6b5016d97b3727e156c4c455cefdbd4e3f11
Innectic <innectic@gmail.com>
2016-09-30 11:40:31 -0700

    Finish basic logging handler.

--- cactusbot/handlers/logging.py
+++ cactusbot/handlers/logging.py
@@ -2,5 +2,16 @@
 
 from ..handler import Handler
 
+import logging
+
 class LoggingHandler(Handler):
-    pass
+    """Logging handler."""
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def on_log(self, packet):
+        """Handle logging events."""
+        # XXX: This is subject to change. We'll have to see what the data structure ends up as.
+        self.logger.info(packet["message"])
+        self.logger.debug(packet)
