d2466aeb061a62cc961c02578aa1339c48414b1f
2Cubed <2Cubed@users.noreply.github.com>
2016-10-05 01:20:11 -0400

    Remove BeamHandler from __init__'s

--- cactusbot/services/__init__.py
+++ cactusbot/services/__init__.py
@@ -2,6 +2,4 @@
 from .api import API
 from .websocket import WebSocket
 
-from .beam import BeamHandler
-
-__all__ = ["API", "WebSocket", "BeamHandler"]
+__all__ = ["API", "WebSocket"]
--- cactusbot/services/beam/__init__.py
+++ cactusbot/services/beam/__init__.py
@@ -1,8 +1,7 @@
 """Interact with Beam."""
 
-from .handler import BeamHandler
 from .api import BeamAPI
 from .chat import BeamChat
 from .liveloading import BeamLiveloading
 
-__all__ = ["BeamHandler", "BeamAPI", "BeamChat", "BeamLiveloading"]
+__all__ = ["BeamAPI", "BeamChat", "BeamLiveloading"]
