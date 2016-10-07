c6d7a7a84e8a53c527f7c3ef93ec16c62781be69
Innectic <innectic@gmail.com>
2016-10-07 11:39:31 -0700

    Fix init

--- cactusbot/services/beam/__init__.py
+++ cactusbot/services/beam/__init__.py
@@ -2,6 +2,7 @@
 
 from .api import BeamAPI
 from .chat import BeamChat
+from .handler import BeamHandler
 from .constellation import BeamConstellation
 
 __all__ = ["BeamHandler", "BeamAPI", "BeamChat", "BeamConstellation"]
