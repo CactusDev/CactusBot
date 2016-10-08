c8ea051287afb51a6bccd92ecf8dddc0d7434a3e
2Cubed <2Cubed@users.noreply.github.com>
2016-10-08 13:41:36 -0400

    Clean up EventHandler

--- cactusbot/handlers/events.py
+++ cactusbot/handlers/events.py
@@ -7,24 +7,27 @@ from ..packets import MessagePacket
 class EventHandler(Handler):
     """Events handler."""
 
+    def on_follow(self, packet):
+        """Handle follow packets."""
+        if packet.success:
+            return MessagePacket(
+                # TODO: Make configurable
+                ("text", "Thanks for following, @{} !".format(packet.user)),
+                user="BOT USER"
+            )
+
     def on_subscribe(self, packet):
         """Handle subscription packets."""
         return MessagePacket(
-            ("text", "Thanks for subscribing, @{} !".format(packet.json["user"])),
-            user="BOT USER"
-        )
-
-    def on_follow(self, packet):
-        """Handle follow packets."""
-        return MessagePacket(
-            ("text", "Thanks for following the channel, @{} !".format(
-                packet.json["user"])),
+            # TODO: Make configurable
+            ("text", "Thanks for subscribing, @{} !".format(packet.user)),
             user="BOT USER"
         )
 
     def on_host(self, packet):
         """Handle host packets."""
         return MessagePacket(
-            ("text", "Thanks for hosting the channel, @{} !".format(packet.json["user"])),
+            # TODO: Make configurable
+            ("text", "Thanks for hosting, @{} !".format(packet.user)),
             user="BOT USER"
         )
