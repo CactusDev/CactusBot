67664833d113a745cc168389349659219851711c
Innectic <innectic@gmail.com>
2016-10-07 22:57:10 -0700

    Update config template

--- config.template.py
+++ config.template.py
@@ -1,10 +1,15 @@
 """CactusBot configuration."""
 
-from cactusbot.services import BeamHandler
+from cactusbot.services.beam.handler import BeamHandler
+
+from cactusbot.handler import Handlers
+from cactusbot.handlers import CommandHandler, LoggingHandler, SpamHandler, EventHandler
+
 
 USERNAME = "BotUsername"
 PASSWORD = "p455w0rd"
 
 CHANNEL = "ChannelName"
+handlers = Handlers(LoggingHandler(), EventHandler(), CommandHandler(CHANNEL), SpamHandler())
 
-SERVICE = BeamHandler(CHANNEL)
+SERVICE = BeamHandler(CHANNEL, handlers)
