a898e3aa812406fb3f751085b68e0ef5157124fd
2Cubed <2Cubed@users.noreply.github.com>
2016-10-14 00:30:09 -0400

    Fix LoggingHandler

--- cactusbot/handlers/logging.py
+++ cactusbot/handlers/logging.py
@@ -12,24 +12,24 @@ class LoggingHandler(Handler):
 
     def on_join(self, packet):
         """Handle user join events."""
-        self.logger.info("%s joined", packet)
+        self.logger.info("%s joined", packet.user)
 
     def on_leave(self, packet):
         """Handle user join events."""
-        self.logger.info("%s left", packet)
+        self.logger.info("%s left", packet.user)
 
     def on_follow(self, packet):
         """Handle follow events."""
-        self.logger.info("%s followed", packet)
+        self.logger.info("%s followed", packet.user)
 
     def on_subscribe(self, packet):
         """Handle subscription events."""
-        self.logger.info("%s subscribed", packet)
+        self.logger.info("%s subscribed", packet.user)
 
     def on_resubscribe(self, packet):
         """Handle resubscription events."""
-        self.logger.info("%s resubscribed", packet)
+        self.logger.info("%s resubscribed", packet.user)
 
     def on_host(self, packet):
         """Handle host events."""
-        self.logger.info("%s hosted", packet)
+        self.logger.info("%s hosted", packet.user)
