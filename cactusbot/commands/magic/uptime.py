793eb007f362e884d77d8ab185436ee5ea4459f6
2Cubed <2Cubed@users.noreply.github.com>
2016-11-03 15:56:12 -0400

    Fix !uptime command

--- cactusbot/commands/magic/uptime.py
+++ cactusbot/commands/magic/uptime.py
@@ -1,24 +1,27 @@
 """Uptime command."""
 
-from datetime import datetime
-
-from . import Command
+import datetime
 
 import aiohttp
+
+from . import Command
 
 
 class Uptime(Command):
     """Uptime command."""
 
+    BEAM_MANIFEST_URL = ("https://beam.pro/api/v1/channels/{channel}"
+                         "/manifest.light2")
+
     @Command.subcommand
     async def get(self, *, channel: "channel"):
         data = await (await aiohttp.get(
-            "https://beam.pro/api/v1/channels/{channel}/manifest.light2"
-            .format(channel=channel))).json()
+            self.BEAM_MANIFEST_URL.format(channel=channel)
+        )).json()
 
         if "startedAt" in data:
-            time = datetime.utcnow() - datetime.strptime(
-                        data["startedAt"], "%Y-%m-%dT%H:%M:%S.%f")
+            time = datetime.datetime.utcnow() - datetime.datetime.strptime(
+                data["startedAt"], "%Y-%m-%dT%H:%M:%S.%fZ")
             time -= datetime.timedelta(microseconds=time.microseconds)
             return "Channel has been live for {}.".format(time)
 
