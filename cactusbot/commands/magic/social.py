7fe875f813015ee8e3c8ae2c995dccb90bccaa41
Innectic <innectic@gmail.com>
2016-12-26 14:07:20 -0800

    Add awaits

--- cactusbot/commands/magic/social.py
+++ cactusbot/commands/magic/social.py
@@ -46,7 +46,7 @@ class Social(Command):
     async def add(self, service, url):
         """Add a social service."""
 
-        response = self.api.add_social(service, url)
+        response = await self.api.add_social(service, url)
         if response.status == 201:
             return "Added social service {}.".format(service)
         elif response.status == 200:
@@ -56,7 +56,7 @@ class Social(Command):
     async def remove(self, service):
         """Remove a social service."""
 
-        response = self.api.remove_social(service)
+        response = await self.api.remove_social(service)
         if response.status == 200:
             return "Removed social service {}.".format(service)
         elif response.status == 404:
