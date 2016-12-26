f01fa05f3a4c0c3615350072186bd41f61e6182c
2Cubed <2Cubed@users.noreply.github.com>
2016-12-25 23:08:42 -0500

    Fix execution path

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,11 @@
+import os
+import sys
+
 from sphinx.apidoc import main
 
 from recommonmark.parser import CommonMarkParser
+
+sys.path.insert(0, os.path.abspath('.'))
 
 source_parsers = {
     '.md': CommonMarkParser,
