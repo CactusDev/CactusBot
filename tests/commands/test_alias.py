710f0c787be9d1d9495992273450afe73a53b91a
RPiAwesomeness <keyboardmailtesting@gmail.com>
2017-03-25 16:32:27 -0400

    We did a dumb. Code is now 42% less dumb

--- tests/commands/test_alias.py
+++ tests/commands/test_alias.py
@@ -197,12 +197,14 @@ class MockAPI:
 
 alias = Alias(MockAPI())
 
+
 @pytest.mark.asyncio
 async def test_create_alias():
     """Create an alias."""
     assert (await alias("add", "test", "testing", packet=MessagePacket(
         "!alias add test testing", role=5))
-           ) == "Alias !test for command !testing updated."
+    ) == "Alias !test for command !testing updated."
+
 
 @pytest.mark.asyncio
 async def test_remove_alias():
@@ -210,8 +212,11 @@ async def test_remove_alias():
     assert (await alias("remove", "test", packet=MessagePacket(
         "!alias remove test", role=5))) == "Alias !test removed."
 
+
 @pytest.mark.asyncio
 async def test_list_alias():
-    """Lis aliases."""
+    """List aliases."""
+    await alias("add", "test", "testing", packet=MessagePacket(
+        "!alias add test testing", role=5))
     assert (await alias("list", packet=MessagePacket(
         "!alias list", role=5))) == "Aliases: test (testing)."
