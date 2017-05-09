2fdf493c0429a582329ea03773ca4d2ce2fbaee0
2Cubed <2Cubed@users.noreply.github.com>
2017-05-09 17:35:31 -0400

    Important business logic
    Hopefully to unbreak Coveralls build...?

--- cactusbot/commands/magic/__init__.py
+++ cactusbot/commands/magic/__init__.py
@@ -6,16 +6,15 @@ from .cactus import Cactus
 from .command import Meta
 from .config import Config
 from .cube import Cube, Temmie
+from .multi import Multi
 from .quote import Quote
 from .repeat import Repeat
 from .social import Social
 from .trust import Trust
 from .uptime import Uptime
-from .multi import Multi
 
 COMMANDS = (Alias, Cactus, Meta, Config, Cube, Temmie,
             Quote, Repeat, Social, Trust, Uptime, Multi)
 
 __all__ = ("Alias", "Command", "Cactus", "Meta", "Config", "Cube",
-           "Temmie", "Quote", "Repeat", "Social", "Trust", "Uptime",
-           "Multi")
+           "Temmie", "Quote", "Repeat", "Social", "Trust", "Uptime", "Multi")
